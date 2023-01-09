991ee8a9cda10b3626736099c3087086a5103e35
Baron King <bkkcreative16@gmail.com>
2023-01-09 17:37:23 -0600

    fixed

--- src/server/stripe/client.ts
+++ src/server/stripe/client.ts
@@ -1,9 +1,8 @@
 import Stripe from "stripe";
-import { env } from "../../env/server.mjs";
 
 export const stripe = new Stripe(
   "sk_test_51KsrI3Lojhc6RaUL1tjZKe79wdB90glSQikDS3FnhsSYpLFVYcXQGpTC2mHG15X0sFmjqSigyDeZpOxV7A8qCwvi00Sqd8fjmO",
   {
-    apiVersion: "2022-08-01",
+    apiVersion: "2022-11-15",
   }
 );
